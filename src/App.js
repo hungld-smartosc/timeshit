96eca97e5e1e84f9cadc5aff9749b7b0de21f76d
hungld <hungld1@smartosc.com>
2022-07-15 02:12:01 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,322 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const MONTHS = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+const today = new Date();
+const currentMonth = MONTHS[today.getMonth()];
+const currentDay = getDay(today);
+
 function App() {
+  const [_projects, setProjects] = useState([]);
+  const [_tasks, setTasks] = useState([]);
+  const [_user, setUser] = useState('');
+  const [_project, pickProject] = useState('');
+  const [_task, pickTask] = useState('');
+  const [cookies, setCookies] = useState({});
+  const [_shitDays, setShitdays] = useState({
+    needAttendance: {},
+    needLog: {},
+  });
+  // const _iamadt = localStorage.getItem('_iamadt');
+  // const _iambdt = localStorage.getItem('_iambdt');
+  // const CSRF_TOKEN = localStorage.getItem('CSRF_TOKEN');
+  // console.log('#####', { _iamadt, _iambdt, CSRF_TOKEN });
+
+  useEffect(() => {
+    init({
+      setProjects,
+      setTasks,
+      setUser,
+      setShitdays,
+      pickProject,
+      pickTask,
+      cookies,
+    });
+  }, [cookies]);
+  const handleProjectChange = (e) => {
+    pickProject(e.target.value);
+  };
+  const handleTaskChange = (e) => {
+    pickTask(e.target.value);
+  };
+  const handleInputChange = (e) => {
+    setCookies((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    // if (!cookies._iamadt && !cookies._iambdt && !cookies.CSRF_TOKEN) {
+    //   localStorage.setItem([e.target.name], e.target.value);
+    // }
+  };
+
+  const handleLog = async () => {
+    const logTimeSheetPromises = [];
+    for (const day in _shitDays.needLog) {
+      const body = {
+        projectId: _project,
+        taskId: _task,
+        userId: _user,
+        date: day,
+        hour: _shitDays.needLog[day],
+      };
+      logTimeSheetPromises.push(logTimeSheet(cookies, body));
+    }
+    await Promise.all(logTimeSheetPromises);
+    await init({
+      setProjects,
+      setTasks,
+      setUser,
+      setShitdays,
+      pickProject,
+      pickTask,
+      cookies,
+    });
+    alert('Done');
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>_iamadt</label>
+      <input
+        type="text"
+        name="_iamadt"
+        value={cookies._iamadt || ''}
+        onChange={handleInputChange}
+      />
+      <br />
+      <label>_iambdt</label>
+      <input
+        type="text"
+        name="_iambdt"
+        value={cookies._iambdt || ''}
+        onChange={handleInputChange}
+      />
+      <br />
+      <label>CSRF_TOKEN</label>
+      <input
+        type="text"
+        name="CSRF_TOKEN"
+        value={cookies.CSRF_TOKEN || ''}
+        onChange={handleInputChange}
+      />
+      <br />
+      <label for="Project">Project:</label>
+      <select
+        value={_project}
+        name="Project"
+        id="Project"
+        onChange={handleProjectChange}
+      >
+        {_projects.map((project) => (
+          <option value={project.Id}>{project.Value}</option>
+        ))}
+      </select>
+      <br />
+      <label for="Task">Task:</label>
+      <select value={_task} name="Task" id="Task" onChange={handleTaskChange}>
+        {_tasks.map((task) => (
+          <option value={task.Id}>{task.Value}</option>
+        ))}
+      </select>
+      <br />
+
+      <div>
+        <b>Các ngày cần log timesheet:</b>
+        <ol>
+          {Object.keys(_shitDays.needLog).map((day) => (
+            <li key={day}>
+              {day}: {_shitDays.needLog[day]}h
+            </li>
+          ))}
+        </ol>
+        <button onClick={handleLog}>Log</button>
+        <br />
+        <b>Các ngày cần xin attendance:</b>
+        <ol>
+          {Object.keys(_shitDays.needAttendance).map((day) => (
+            <li key={day}>
+              {day}: {_shitDays.needAttendance[day]}h
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
 
+async function init({
+  setUser,
+  setProjects,
+  setTasks,
+  setShitdays,
+  pickProject,
+  pickTask,
+  cookies,
+}) {
+  const { tasks, projects, userId } = await fetchTasksAndProjects(cookies);
+  setUser(userId);
+  setProjects(projects);
+  setTasks(tasks);
+  //Set default project and task
+  pickProject(projects[0].Id);
+  pickTask(tasks[0].Id);
+  const shitDays = await getShitDays(cookies);
+  setShitdays(shitDays);
+}
+
+// API
+async function fetchAttendance(cookie) {
+  const { _iamadt, _iambdt, CSRF_TOKEN } = cookie;
+  const response = await fetch(
+    'https://people.zoho.com/hrportal1524046581683/AttendanceViewAction.zp',
+    {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        cookie: `_iamadt=${_iamadt}; _iambdt=${_iambdt}; CSRF_TOKEN=${CSRF_TOKEN};`,
+      },
+      body: `mode=getAttList&conreqcsr=${CSRF_TOKEN}&loadToday=false&view=month&preMonth=0`,
+      method: 'POST',
+    }
+  );
+  const { dayList } = await response.json();
+  const dayAndTime = {};
+  for (const index in dayList) {
+    const day = dayList[index];
+    if (day.status === 'Weekend') continue;
+    dayAndTime[day.ldate] = parseTime(day.tHrs);
+  }
+  return dayAndTime;
+}
+
+async function fetchTasksAndProjects(cookie) {
+  const { _iamadt, _iambdt, CSRF_TOKEN } = cookie;
+  const response = await fetch(
+    'https://people.zoho.com/hrportal1524046581683/formAction.zp',
+    {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        cookie: `_iamadt=${_iamadt}; _iambdt=${_iambdt}; CSRF_TOKEN=${CSRF_TOKEN};`,
+      },
+      body: `formId=412762000003736053&mode=getFormComponents&subMode=ADD&conreqcsr=${CSRF_TOKEN}`,
+      method: 'POST',
+    }
+  );
+  const {
+    message: {
+      formComponentDetails: [
+        {
+          column1: [
+            {
+              AddOptions: [{ Id: userId }],
+            },
+            ,
+            ,
+            { Options: Tasks },
+          ],
+          column2: [{ Options: Projects }],
+        },
+      ],
+    },
+  } = await response.json();
+  return {
+    userId,
+    tasks: Tasks,
+    projects: Projects,
+  };
+}
+
+async function getLoggedDay(cookie) {
+  const { _iamadt, _iambdt, CSRF_TOKEN } = cookie;
+  const response = await fetch(
+    'https://people.zoho.com/hrportal1524046581683/viewAction.zp',
+    {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        cookie: `_iamadt=${_iamadt}; _iambdt=${_iambdt}; CSRF_TOKEN=${CSRF_TOKEN};`,
+      },
+      body: `mode=fetchRecords&formId=412762000003736053&viewId=412762000003736055&isOnload=false&typeOfView=self&startInd=0&limit=30&conreqcsr=${CSRF_TOKEN}`,
+      method: 'POST',
+    }
+  );
+  const {
+    recordDetails: {
+      message: { recordDetails },
+    },
+  } = await response.json();
+  return recordDetails
+    .filter(
+      (record) => record.approvalStatus === 1 || record.approvalStatus === -1
+    )
+    .map((record) => record.fieldDetails[2])
+    .filter((day) => day.includes(currentMonth));
+}
+
+async function getShitDays(cookie) {
+  const needLog = {};
+  const needAttendance = {};
+  const attendance = await fetchAttendance(cookie);
+  const loggedDays = await getLoggedDay(cookie);
+  loggedDays.forEach((loggedDay) => delete attendance[loggedDay]);
+  for (const remainDay in attendance) {
+    if (remainDay < currentDay) {
+      if (attendance[remainDay] < 6) {
+        needAttendance[remainDay] = attendance[remainDay];
+      } else {
+        needLog[remainDay] = attendance[remainDay];
+      }
+    } else {
+      delete attendance[remainDay];
+    }
+  }
+  return { needLog, needAttendance };
+}
+
+async function logTimeSheet(cookie, body) {
+  const { _iamadt, _iambdt, CSRF_TOKEN } = cookie;
+  // Date:  04-Jul-2022
+  const { projectId, taskId, userId, hour, date } = body;
+  await fetch(
+    'https://people.zoho.com/hrportal1524046581683/addUpdateRecord.zp',
+    {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        cookie: `_iamadt=${_iamadt}; _iambdt=${_iambdt}; CSRF_TOKEN=${CSRF_TOKEN};`,
+      },
+      body: `isPicklistIdEnabled=true&Emp_info=${userId}&Task=${taskId}&Timesheet_Hours=${hour}&Project=${projectId}&Work_location=412762000010757909&Date=${date}&Task_Description=&Billable_Hours=0&zp_tableName=t_412762000003736053&conreqcsr=${CSRF_TOKEN}&zp_formId=412762000003736053&zp_mode=addRecord&isDraft=false&isResubmit=false`,
+      method: 'POST',
+    }
+  );
+}
+//////////////////////////////////////////////////////
+
+// Helper
+function roundHalf(num) {
+  return Math.round(num * 2) / 2;
+}
+
+function parseTime(time) {
+  const [hour, minute] = time.split(':');
+  return roundHalf(Number(hour) + Number(minute) / 60);
+}
+
+function getDay(date) {
+  let d = date.toISOString().split('T')[0].split('-');
+  const m = Number(d[1]) - 1;
+  d[1] = MONTHS[m];
+  [d[0], d[2]] = [d[2], d[0]];
+  return d.join('-');
+}
+/////////////////////////////////////////////////////
 export default App;
